3b4722108cb299a0553a449ccb3b8b57b5d8ce11
bigcoder69 <124915579+bigcoder69@users.noreply.github.com>
2023-03-15 12:08:16 -0400

    familycounter.py
    This family counter counts the number of boys and girls are there in the family, then gives you the result!
    If you have very little experience this great because it goes over core topics for your python learning journey. Happy coding!

---
+++ .github/workflows/familycounter.py
@@ -0,0 +1,14 @@
+#Family counter
+
+#The function for the family counter
+def family_counter():
+    #The variables for the family counter
+    boys = int(input("Enter number of boys: "))
+    girls = int(input("Enter number of girls: "))
+    #The print for the family counter
+    print ("Wow thats a good sized family!")
+    print(boys + girls)
+    #The forever loop for the family counter
+    while True:
+
+        family_counter()
